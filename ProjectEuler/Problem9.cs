1c86782ff203c30e777960ce1769b264f5830b0d
Abdurrahman S <a.sibahi@gmail.com>
2016-03-20 18:58:38 -0500

    Problem 9

---
+++ ProjectEuler/Problem9.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+	static class Problem9
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Project Euler - 9: Special Pythagorean triplet");
+			var r = 1d;
+			var sw = new Stopwatch();
+			sw.Start();
+
+			var limit = 1000;
+			for (double a = 1; r == 1 && a < limit; a++)
+			{
+				for (double b = a; r == 1 && b < limit; b++)
+				{
+					for (double c = b; r == 1 && c < limit; c++)
+					{
+						if ((a * a) + (b * b) == (c * c) && a + b + c == 1000)
+						{
+							r = a * b * c;
+						}
+					}
+				}
+			}
+			sw.Stop();
+			Console.WriteLine("Time elapsed:	{0}", sw.ElapsedMilliseconds);
+			Console.WriteLine("Result:		{0}", r);
+			Console.ReadKey();
+
+		}
+	}
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			Problem7.Run();
+			Problem9.Run();
 
 		}
 	}
