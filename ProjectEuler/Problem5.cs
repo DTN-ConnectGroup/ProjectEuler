afc3be5107db811ad40dfb4c486846847db1e163
Abdurrahman S <a.sibahi@gmail.com>
2016-03-20 17:16:01 -0500

    Problem 5

---
+++ ProjectEuler/Problem5.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+	static class Problem5
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Project Euler - 5: Smallest Multiple");
+			var r = 1;
+			var sw = new Stopwatch();
+			sw.Start();
+
+			var numbers = new int[20];
+			for (int i = 0; i < 20; i++)
+				numbers[i] = i + 1;
+			
+
+			var resolved = false;
+
+			for (int i = 20; !resolved; i+= 20)
+			{
+				resolved = true;
+				r = i;
+				foreach (int n in numbers)
+				{
+					resolved &= i % n == 0;
+				}
+			}
+
+			sw.Stop();
+			Console.WriteLine("Time elapsed: " + sw.ElapsedMilliseconds);
+			Console.WriteLine("The solution is {0}", r);
+			Console.ReadKey();
+		}
+	}
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			Problem4.Run();
+			Problem5.Run();
 
 		}
 	}
