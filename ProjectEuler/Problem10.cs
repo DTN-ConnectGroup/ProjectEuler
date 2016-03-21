c617b67ac56a824df228091546c14320ed70985b
Abdurrahman S <a.sibahi@gmail.com>
2016-03-21 00:19:15 -0500

    House keeping and added a file for Problem 10

---
+++ ProjectEuler/Problem10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+	static class Problem10
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Project Euler - 9: Special Pythagorean triplet");
+			var r = 1d;
+			var sw = new Stopwatch();
+			sw.Start();
+
+
+			sw.Stop();
+			Console.WriteLine("Time elapsed:	{0}", sw.ElapsedMilliseconds);
+			Console.WriteLine("Result:		{0}", r);
+			Console.ReadKey();
+
+		}
+	}
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			Problem8.Run();
+			Problem10.Run();
 
 		}
 	}
