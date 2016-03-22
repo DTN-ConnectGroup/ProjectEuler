88370fa48720d3d46855f60581dc830ebd1c6d6a
Abdurrahman S <a.sibahi@gmail.com>
2016-03-22 01:23:20 -0500

    Problem 12 + Problem 13 draft

---
+++ ProjectEuler/Problem12.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Project_Euler
+{
+	static class Problem12
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Project Euler - 12: Highly divisible triangular number");
+			double r = 0;
+			var sw = new Stopwatch();
+			sw.Start();
+
+			bool isFive = false;
+
+			for (int i = 1; !isFive; i++)
+			{
+				r += i;
+				isFive = DivisorCount(r) > 500;
+			}
+
+			sw.Stop();
+			Console.WriteLine("Time elapsed:	{0}", sw.ElapsedMilliseconds);
+			Console.WriteLine("Result:		{0}", r);
+			Console.ReadKey();
+
+		}
+
+		static int DivisorCount(double x)
+		{
+			var val = 0;
+			for (int i = 1; i * i <= x; i++)
+			{
+				if (x % i == 0)
+					val += 2;
+			}
+			return val;
+		}
+	}
+}
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -4,7 +4,7 @@
 	{
 		static void Main(string[] args)
 		{
-			Problem11.Run();
+			Problem13.Run();
 
 		}
 	}
