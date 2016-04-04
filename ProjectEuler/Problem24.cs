ee4193cb1af4cb60edf90d5199800f43d184848e
Abdurrahman S <a.sibahi@gmail.com>
2016-04-03 21:58:35 -0500

    Problem 24 placeholder

---
+++ ProjectEuler/Problem24.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler
+{
+	static class Problem24
+	{
+		public static void Run()
+		{
+			Console.WriteLine("Project Euler - 24: Lexicographic permutations");
+			double r = 0;
+			var sw = new Stopwatch();
+			sw.Start();
+			
+			// Placeholder
+
+			sw.Stop();
+			Console.WriteLine($"Time elapsed:	{sw.ElapsedMilliseconds}ms");
+			Console.WriteLine($"Result:		{r}");
+			Console.ReadKey();
+		}
+	}
+}
