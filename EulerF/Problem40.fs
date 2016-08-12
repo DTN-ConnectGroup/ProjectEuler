7c1c269c72e817411070947ce34eb020b52a4b4c
asibahi <a.sibahi@gmail.com>
2016-08-11 22:48:40 -0500

    Problem 40

---
+++ EulerF/Problem40.fs
@@ -0,0 +1,14 @@
+﻿module EulerF.Problem40
+
+let FractionalPartSeq = 
+    seq { 
+        for i in 0..System.Int32.MaxValue do
+            yield! (Helpers.Digits i |> Seq.rev)
+    }
+
+let NthDigit n = FractionalPartSeq |> Seq.item n
+
+let Solution = 
+    Seq.init 7 (fun i -> int <| 10.0 ** (float i))
+    |> Seq.map NthDigit
+    |> Seq.reduce (*)
---
+++ ProjectEuler/Problem40.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project_Euler
+{
+    static class Problem40
+    {
+        public static int Run()
+        {
+            Console.WriteLine("Project Euler - 40: Champernowne's constant");
+
+            return EulerF.Problem40.Solution;
+        }
+    }
+}
