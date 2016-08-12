32b165fe718140093614c9fbb97a260bb6f04192
asibahi <a.sibahi@gmail.com>
2016-08-11 22:49:48 -0500

    Comment

--- EulerF/Problem40.fs
+++ EulerF/Problem40.fs
@@ -11,4 +11,4 @@ let NthDigit n = FractionalPartSeq |> Seq.item n
 let Solution = 
     Seq.init 7 (fun i -> int <| 10.0 ** (float i))
     |> Seq.map NthDigit
-    |> Seq.reduce (*)
+    |> Seq.reduce (*) // a bit too fancy way to do it
